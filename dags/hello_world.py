1a6379e4350c2537ec9991d378528443baa9b768
khaidao2 <daok257@gmail.com>
2026-05-26 16:50:30 +0700

    feat: add hello world DAG

---
+++ dags/hello_world.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'hello_world_dag',
+    default_args=default_args,
+    description='A simple hello world DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2026, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    t1 = BashOperator(
+        task_id='print_date',
+        bash_command='date',
+    )
+
+    t2 = BashOperator(
+        task_id='sleep',
+        depends_on_past=False,
+        bash_command='sleep 5',
+        retries=3,
+    )
+
+    t3 = BashOperator(
+        task_id='templated',
+        depends_on_past=False,
+        bash_command="""
+            echo "hello world"
+            echo "ds={{ ds }}"
+        """,
+    )
+
+    t1 >> [t2, t3]
